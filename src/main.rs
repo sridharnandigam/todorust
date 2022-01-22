e0c1e4f45619e3c12e8e2436bf248a02bc1b414a
Sridhar Nandigam <nandigamsridhar.v@gmail.com>
2022-01-22 14:45:33 -0600

    vector of commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
 #![allow(non_snake_case)]
+#![allow(unused)]
+
+use clap::Parser;
+use std::env;
+/*
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli{
+    #[clap(short, long)]
+    pattern: String,
+
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+*/
+
 fn main() {
-    println!("Hello, world!");
+    //add task
+    //remove task
+    //edit task
+    //complete task
+    //list tasks
+    //help (list commands)
+    //let args = Cli::parse();
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("{:?}", args);
 }
