8259d9e89b34206688fa9168f1fb7372e7068dc4
Sridhar Nandigam <nandigamsridhar.v@gmail.com>
2022-01-26 16:39:38 -0600

    figured out arggroup and error handling

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 #![allow(unused)]
 
 use clap::Parser;
+use clap::{Arg, App, ArgGroup};
 use std::env;
-/*
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli{
@@ -13,18 +14,50 @@ struct Cli{
     #[clap(short, long, default_value_t = 1)]
     count: u8,
 }
-*/
+
+struct TodoList{
+    name: String,
+    items: Vec<String>
+}
+
+impl TodoList{
+    fn new(newName: String, newItems: Vec<String>) -> Self {
+        TodoList {name: newName, items: newItems}
+    }
+}
 
 fn main() {
-    //add task
-    //remove task
-    //edit task
-    //complete task
-    //list tasks
-    //help (list commands)
     //let args = Cli::parse();
+    let app = App::new("todo")
+        .version("1.0")
+        .about("Store daily todo list")
+        .author("Sridhar Nandigam");
 
-    let args: Vec<String> = env::args().collect();
+    let name_option = Arg::new("name")
+        .long("name")
+        .takes_value(true)
+        .help("Who to say hello to");
 
-    println!("{:?}", args);
+    let add_item = Arg::new("additem")
+        .long("additem")
+        .takes_value(true)
+        .short('a');
+
+    let app = app.arg(name_option)
+                .arg(add_item)
+                .group(ArgGroup::new("options")
+                    .args(&["name", "additem"])
+                    .required(true));
+
+    let matches = app.get_matches();
+        
+    if(matches.is_present("name")){
+        let name = matches.value_of("name").unwrap();
+        println!("Got name: {name}")
+    } else if(matches.is_present("additem")){
+        let item = matches.value_of("additem").unwrap();
+        println!("Received item: {item}");
+    } else{
+        println!("Bruh.....");
+    }
 }
