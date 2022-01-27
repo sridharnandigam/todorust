037f458e722a6c26ff42a7568f79afc422f6ad2b
Sridhar Nandigam <nandigamsridhar.v@gmail.com>
2022-01-27 13:09:28 -0600

    save functionality easier than i thought

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use std::env;
 use std::collections::HashMap;
 use chrono;
 
+const DATE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli{
@@ -33,8 +35,34 @@ impl TodoList{
     }
 
     fn print(&self){
-        println!("{} - {}", self.name, self.date.format("%Y-%m-%d %H:%M:%S").to_string());
+        println!("{} - {}", self.name, self.date.format(&DATE_FORMAT).to_string());
     }
+
+    fn save(&self) -> String{
+        let mut content = String::new();
+
+        content.push_str(&self.name);
+        content.push_str("\n");
+        content.push_str(&self.date.format(&DATE_FORMAT).to_string());
+
+        for (k, v) in &self.items{
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        println!("{}", content);
+        content
+    }
+}
+
+fn save_to_file(input: &Vec<TodoList>) -> Result<(), std::io::Error>{
+    let mut output = String::new();
+    for x in input{
+        let record = x.save();
+        output.push_str(&record);
+        output.push_str("--");
+    }
+
+    std::fs::write("cache.txt", output)
 }
 
 fn main() {
@@ -58,13 +86,20 @@ fn main() {
         .takes_value(true)
         .short('a')
         .help("Add item to list");
+
+    let view_all = Arg::new("viewall")
+        .long("viewall")
+        .takes_value(false)
+        .short('v')
+        .help("View all lists");
     
     
 
     let app = app.arg(new_list)
                 .arg(add_item)
+                .arg(view_all)
                 .group(ArgGroup::new("options")
-                    .args(&["newlist", "additem"])
+                    .args(&["newlist", "additem", "viewall"])
                     .required(true));
 
     let matches = app.get_matches();
@@ -76,12 +111,23 @@ fn main() {
         list_vec.push(new_todo);
 
         for x in &list_vec {
-            x.print();
+            x.save();
         }
+        println!("{}", list_vec.len())
     } else if(matches.is_present("additem")){
         let items: Vec<&str> = matches.values_of("additem").unwrap().collect();
         println!("Received item: {:?}", items);
+    } else if(matches.is_present("viewall")){
+        if(list_vec.len() == 0){
+            println!("No lists present")
+        } else{
+            for x in &list_vec {
+                x.print();
+            }
+        }
     } else{
-        println!("Bruh.....");
+        println!("Bruh, how tf.....");
     }
+
+    save_to_file(&list_vec);
 }
